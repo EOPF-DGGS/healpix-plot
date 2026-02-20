e9ee46f8f9e609e365f502df269c3ade06343387
Justus Magin <keewis@posteo.de>
2026-02-20 12:28:25 +0100

    tests for the parametrized sampling grid

--- healpix_plotting/tests/test_sampling_grid.py
+++ healpix_plotting/tests/test_sampling_grid.py
@@ -5,6 +5,61 @@ from affine import Affine
 from healpix_plotting import sampling_grid as sg
 
 
+class TestParametrizedSamplingGrid:
+    @pytest.mark.parametrize(
+        ["shape", "expected_shape"], ((3, (3, 3)), ((2, 5), (2, 5)))
+    )
+    @pytest.mark.parametrize(
+        ["resolution", "expected_resolution"],
+        ((1.1, (1.1, 1.1)), ((0.5, 1.2), (0.5, 1.2)), (None, None)),
+    )
+    @pytest.mark.parametrize(
+        ["center", "expected_center"],
+        (((10.1, 13.6), (10.1, 13.6)), ((0.5, 1.2), (0.5, 1.2)), (None, None)),
+    )
+    def test_from_parameters(
+        self,
+        shape,
+        expected_shape,
+        resolution,
+        expected_resolution,
+        center,
+        expected_center,
+    ):
+        actual = sg.ParametrizedSamplingGrid.from_parameters(
+            shape=shape, resolution=resolution, center=center
+        )
+
+        assert actual.shape == expected_shape
+        assert actual.resolution == expected_resolution
+        assert actual.center == expected_center
+
+    def test_from_dict(self):
+        shape = (3, 4)
+        resolution = (0.5, 1.0)
+        center = (45.3, -4.3)
+        actual = sg.ParametrizedSamplingGrid.from_dict(
+            {"shape": shape, "resolution": resolution, "center": center}
+        )
+
+        assert actual.shape == shape
+        assert actual.resolution == resolution
+        assert actual.center == center
+
+    @pytest.mark.parametrize(
+        ["bbox", "shape", "expected_resolution", "expected_center"],
+        (
+            ((0, 0, 5, 5), 10, (5 / 9, 5 / 9), (2.5, 2.5)),
+            ((0, -5, 10, 15), (11, 21), (1.0, 1.0), (5, 5)),
+        ),
+    )
+    def test_from_bbox(self, bbox, shape, expected_resolution, expected_center):
+        actual = sg.ParametrizedSamplingGrid.from_bbox(bbox, shape)
+        assert actual.shape == (shape if isinstance(shape, tuple) else (shape, shape))
+        assert actual.resolution == expected_resolution
+        assert actual.center == expected_center
+
+
 class TestAffineSamplingGrid:
     @pytest.mark.parametrize(
         ["transform", "shape", "expected_shape"],
